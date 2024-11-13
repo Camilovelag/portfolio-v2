d84cb272aff29ce4250a5ea12c1be8b8fbd651f5
Camilovelag <camilo.vela.galindo@gmail.com>
2024-11-13 12:55:21 -0500

    wip: create profile section

---
+++ data/data.ts
@@ -0,0 +1,29 @@
+const skills = ['JavaScript', 'TypeScript','Ruby','Responsive Design','NextJs','Angular','Trabajo en equipo','Resolución de Problemas','Aprendizaje Continuo','Adaptabilidad','Orientación a Objetivos'];
+
+const socialMedia = [
+  {
+    type: 'email',
+    icon: 'some text here',
+    content: 'camilo.vela.galindo@gmail.com'
+  },
+  {
+    type: 'phone',
+    icon: 'some text here',
+    content: '+57 3046821730'
+  },
+  {
+    type: 'linkedin',
+    icon: 'some text here',
+    content: '/camilovelag',
+    link: 'https://www.linkedin.com/in/camilovelag/'
+  },
+  {
+    type: 'github',
+    icon: 'some text here',
+    content: '/camilovelag',
+    link: 'https://github.com/Camilovelag'
+  },
+]
+
+export {skills, socialMedia};
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
+import { PhoneCall, Globe } from 'lucide-react';
 import Image from "next/image";
+import profileImage from "../../public/images/1.jpg";
+import {skills, socialMedia} from "../../data/data";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  console.log(skills);
+
+  return (
+    <div className="bg-black text-white flex flex-col items-center">
+
+      <main className="w-3/4 max-w-screen-md py-20">
+        <section>
+          <div className="w-full flex relative">
+            <div className="w-2/3 flex flex-col z-1">
+              <h1 className="text-4xl font-bold">Hi, I'm <text className='text-[#facc15]'>Camilo</text><br />a Frontend Developer</h1>
+              <div className='my-2 flex gap-2 items-center'>
+                <Globe />
+                <p className="text-sm">Bogotá, Colombia</p>
+              </div>
+
+              <p className="my-10">[Here comes some text introducing yourself (brief)].Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! </p>
+              <div>
+                <h2 className="font-bold text-lg text-[#facc15]">Skillset</h2>
+                <ul className="flex gap-1 flex-wrap w-full">
+                  { skills.map((skill, index) => (
+                    <li>
+                      <span className="inline-flex items-center rounded-full bg-gray-700 px-3 py-1 text-xs font-bold text-white ring-1 ring-inset ring-blue-700/10">
+                        {skill}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+  
+            <Image src={profileImage} alt="foto-perfil" width={300} height={100} className="profile--photo h-full object-contain absolute right-0" />
+
+          </div>
+          <ul className="flex justify-around px-5 w-full items-center py-10">
+                  { socialMedia.map((item, index) => (
+                    <li className='flex gap-2'>
+                      {/* <span className="inline-flex items-center bg-gray-900 rounded-full px-5 py-3 text-xs font-bold text-white ring-1 ring-inset ring-gray-500">
+                        {item.content}
+                      </span> */}
+                      <PhoneCall />
+                      <span className="inline-flex items-center text-sm">
+                        {item.content}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer>
+        <p>Here comes the footer</p>
       </footer>
+
     </div>
   );
 }
