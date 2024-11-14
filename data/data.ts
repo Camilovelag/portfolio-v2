43aff728227089d4f52e76af3d51518a91b99501
Camilovelag <camilo.vela.galindo@gmail.com>
2024-11-14 11:46:11 -0500

    feat: create profile section

--- data/data.ts
+++ data/data.ts
@@ -1,29 +1,22 @@
 const skills = ['JavaScript', 'TypeScript','Ruby','Responsive Design','NextJs','Angular','Trabajo en equipo','Resolución de Problemas','Aprendizaje Continuo','Adaptabilidad','Orientación a Objetivos'];
 
-const socialMedia = [
-  {
-    type: 'email',
-    icon: 'some text here',
+const socialMedia = {
+  'email': {
     content: 'camilo.vela.galindo@gmail.com'
   },
-  {
-    type: 'phone',
-    icon: 'some text here',
+  'phone': {
     content: '+57 3046821730'
   },
-  {
-    type: 'linkedin',
-    icon: 'some text here',
+  'linkedin': {
     content: '/camilovelag',
     link: 'https://www.linkedin.com/in/camilovelag/'
   },
-  {
-    type: 'github',
-    icon: 'some text here',
+  'github': {
     content: '/camilovelag',
     link: 'https://github.com/Camilovelag'
   },
-]
+
+}
 
 export {skills, socialMedia};
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { PhoneCall, Globe } from 'lucide-react';
+import { PhoneCall, Globe, Mail } from 'lucide-react';
 import Image from "next/image";
 import profileImage from "../../public/images/1.jpg";
+import linkedinIcon from "../../public/icons/linkedin-app-white-icon.svg"
+import githubIcon from "../../public/icons/icons8-github.svg"
 import {skills, socialMedia} from "../../data/data";
 
 export default function Home() {
@@ -8,19 +10,19 @@ export default function Home() {
   console.log(skills);
 
   return (
-    <div className="bg-black text-white flex flex-col items-center">
+    <div className="bg-[#1a1e36] text-white flex flex-col items-center">
 
       <main className="w-3/4 max-w-screen-md py-20">
         <section>
           <div className="w-full flex relative">
             <div className="w-2/3 flex flex-col z-1">
-              <h1 className="text-4xl font-bold">Hi, I'm <text className='text-[#facc15]'>Camilo</text><br />a Frontend Developer</h1>
+              <h1 className="text-4xl font-bold">Hi, I&apos;m <text className='text-[#facc15]'>Camilo</text><br />a Frontend Developer</h1>
               <div className='my-2 flex gap-2 items-center'>
                 <Globe />
                 <p className="text-sm">Bogotá, Colombia</p>
               </div>
 
-              <p className="my-10">[Here comes some text introducing yourself (brief)].Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+              <p className="my-10 backdrop-blur-sm bg-gray-800/50 z-10 px-3 py-1">[Here comes some text introducing yourself (brief)].Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
 molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
 numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
 optio, eaque rerum! </p>
@@ -28,7 +30,7 @@ optio, eaque rerum! </p>
                 <h2 className="font-bold text-lg text-[#facc15]">Skillset</h2>
                 <ul className="flex gap-1 flex-wrap w-full">
                   { skills.map((skill, index) => (
-                    <li>
+                    <li key={index}>
                       <span className="inline-flex items-center rounded-full bg-gray-700 px-3 py-1 text-xs font-bold text-white ring-1 ring-inset ring-blue-700/10">
                         {skill}
                       </span>
@@ -41,19 +43,33 @@ optio, eaque rerum! </p>
             <Image src={profileImage} alt="foto-perfil" width={300} height={100} className="profile--photo h-full object-contain absolute right-0" />
 
           </div>
+
           <ul className="flex justify-around px-5 w-full items-center py-10">
-                  { socialMedia.map((item, index) => (
-                    <li className='flex gap-2'>
-                      {/* <span className="inline-flex items-center bg-gray-900 rounded-full px-5 py-3 text-xs font-bold text-white ring-1 ring-inset ring-gray-500">
-                        {item.content}
-                      </span> */}
-                      <PhoneCall />
-                      <span className="inline-flex items-center text-sm">
-                        {item.content}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
+              <li className='flex gap-2'>
+                <Mail />
+                <span className="inline-flex items-center text-sm">{socialMedia.email.content}</span>
+              </li>
+
+              <li className='flex gap-2'>
+                <PhoneCall />
+                <span className="inline-flex items-center text-sm">{socialMedia.phone.content}</span>
+              </li>
+
+              <li className='flex gap-2'>
+                <Image src={linkedinIcon} alt="linkedin-icon" width={24} height={24} />
+                <a href={socialMedia.linkedin.link} target='_blank'>
+                  <span className="inline-flex items-center text-sm">{socialMedia.linkedin.content}</span>
+                </a>
+              </li>
+
+              <li className='flex gap-2'>
+                <Image src={githubIcon} alt="linkedin-icon" width={24} height={24} />
+                <a href={socialMedia.github.link} target='_blank'>
+                  <span className="inline-flex items-center text-sm">{socialMedia.github.content}</span>
+                </a>
+              </li>
+          </ul>
+
         </section>
       </main>
 
